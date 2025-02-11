ce0a5cdc23a9ad81583e0f720269a1c56aacb0b3
armandoxd03 <johnroyducut04@gmail.com>
2025-02-11 13:08:53 +0800

    Resolved conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,64 @@
-import './App.css';
+import { useState } from 'react';
+import { animeList } from './data.js';
 
-function App() {
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const hasNext = index < animeList.length - 1;
+  const hasBack = index > 0;
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function handleBackClick() {
+    if (hasBack) {
+      setIndex(index - 1);
+    } else {
+      setIndex(animeList.length - 1);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let anime = animeList[index];
   return (
-    <div>
-      <h1 style={{ color: 'black' }}>John Roy Ducut</h1>
-      <div id="Container"><p>CPEITEL</p></div>
-    </div>
-    );
+    <>
+    <h1>John Roy C. Ducut</h1>
+    <h3>
+        ({index + 1} of {animeList.length})
+      </h3>
+      <button onClick={handleBackClick}>
+        Back
+      </button>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{anime.name} </i>
+        by {anime.artist}
+      </h2>
+      {showMore && <p>{anime.description}</p>}
+      <img
+        src={anime.url}
+        alt={anime.alt}
+      />
+            <div>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      </div>
+    </>
+  );
 }
 
-export default App;
+
+
+
+
